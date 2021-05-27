bb650268dee2e682b4150048ebcac11a57468088
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-05-27 16:55:02 -0300

    Revisao pt1

---
+++ Revisao/Revisao/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Revisao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string nome = Microsoft.VisualBasic.Interaction.InputBox("Informe o nome", "", "");
+            string Sobrenome = Microsoft.VisualBasic.Interaction.InputBox("Informe o sobrenome", "", "");
+            label1.Text = "Olá tudo bem? " + nome + " " + Sobrenome;
+
+
+        }
+    }
+}

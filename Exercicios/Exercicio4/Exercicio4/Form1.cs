a1e6320ccc196f8df8d8e96336b5d65a7b6b53cd
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-05-27 18:00:20 -0300

    Exercicio 4

---
+++ Exercicios/Exercicio4/Exercicio4/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float BMaior = Convert.ToInt32(textBox1.Text);
+            float BMenor = Convert.ToInt32(textBox2.Text);
+            float Altura = Convert.ToInt32(textBox3.Text);
+            float area = ((BMaior + BMenor)*Altura) / 2;
+            label2.Text = "A area do trapezio é: " + Convert.ToString(area);
+
+
+        }
+    }
+}
